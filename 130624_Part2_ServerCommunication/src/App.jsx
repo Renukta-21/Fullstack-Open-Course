0e8db8da18301e776d987aee1ac2bf5e8ffe3dde
Renukta-21 <edu211004@gmail.com>
2024-06-23 16:32:44 -0600

    Finished important functionality

--- 130624_Part2_ServerCommunication/src/App.jsx
+++ 130624_Part2_ServerCommunication/src/App.jsx
@@ -1,53 +1,71 @@
-import Course from "./Course"
-import Notes from './Notes'
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+import { useState } from "react"
+import { notes } from "./notes"
 
-  /* return <Course courses={courses} /> */
-  return <Notes/>
+function App() {
+
+    const [inValue, setInValue] = useState('')
+    const [notesList, setNotesList] = useState(notes)
+    const [actFilter, setActFilter] = useState(false)
+    const [isImportant, setIsImportant] = useState(false)
+    console.log(actFilter)
+    function handleSubmit(e) {
+        e.preventDefault()
+        const newNote = {
+            content: inValue,
+            id: notesList.length + 1,
+            important: isImportant
+        }
+        //Just another way to update state
+        /* const newList = [...notesList, newNote]
+        setNotesList(newList) */
+        setNotesList(prevList => {
+            return [...prevList, newNote]
+        })
+
+        setInValue('')
+
+    }
+    const handleFilter = () => setActFilter(!actFilter)
+    return (
+        <div>
+            <h2>Notes List</h2>
+            <tt>Fullstack Open Practice - Second Section Course</tt>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Note</th>
+                        <th>Importance</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {notesList.filter(note => {
+                        if (actFilter === true) return note.important == true
+                        return note
+                    }).map(note => <NoteLine key={note.id} note={note} />)}
+                </tbody>
+            </table>
+            <form action="POST" onSubmit={handleSubmit}>
+                <input type="text" value={inValue} autoFocus onChange={e => setInValue(e.target.value)} placeholder="Add a note here" />
+                <div className="check">
+                    <label htmlFor="importance">important</label>
+                    <input type="checkbox" name="importance" id="importance" onChange={(e)=>setIsImportant(e.target.checked)}/>
+                </div>
+                <button>Add Note</button>
+            </form>
+            <div className="middButton"><button onClick={handleFilter}>{actFilter ? 'Show all' : 'Filter important'}</button></div>
+            {/* <button className="middButton">Filter important</button> */}
+            <p>&copy; EDUARDO DANIEL URBINA MARTINEZ &copy;</p>
+        </div>
+    )
 }
 
+function NoteLine({ note }) {
+    const { content, important } = note
+    return (
+        <tr>
+            <td>{content}</td>
+            <td className={important ? 'impCeld' : ''}><span className={note.important ? "important" : 'not-important'}>{note.important ? 'important' : 'not-important'}</span></td>
+        </tr>
+    )
+}
 export default App
