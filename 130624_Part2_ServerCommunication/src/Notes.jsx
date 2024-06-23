9c48d7f131ba58213835618f72fd9d6602210ee6
Renukta-21 <edu211004@gmail.com>
2024-06-22 20:51:09 -0600

    Added filter functionality

--- 130624_Part2_ServerCommunication/src/Notes.jsx
+++ 130624_Part2_ServerCommunication/src/Notes.jsx
@@ -5,8 +5,9 @@ function Notes() {
 
     const [inValue, setInValue] = useState('')
     const [notesList, setNotesList] = useState(notes)
-
-
+    const [actFilter, setActFilter] = useState(false)
+    const [isImportant, setIsImportant] = useState(false)
+    console.log(actFilter)
     function handleSubmit(e) {
         e.preventDefault()
         const newNote = {
@@ -17,9 +18,14 @@ function Notes() {
         //Just another way to update state
         /* const newList = [...notesList, newNote]
         setNotesList(newList) */
-        setNotesList(prevList => [...prevList, newNote])
+        setNotesList(prevList => {
+            return [...prevList, newNote]
+        })
+
         setInValue('')
+
     }
+    const handleFilter = () => setActFilter(!actFilter)
     return (
         <div>
             <h2>Notes List</h2>
@@ -32,24 +38,33 @@ function Notes() {
                     </tr>
                 </thead>
                 <tbody>
-                    {notesList.map(note=><NoteLine key={note.id} note={note}/>)}
+                    {notesList.filter(note => {
+                        if (actFilter === true) return note.important == true
+                        return note
+                    }).map(note => <NoteLine key={note.id} note={note} />)}
                 </tbody>
             </table>
             <form action="POST" onSubmit={handleSubmit}>
-                <input type="text"value={inValue} autoFocus onChange={e=>setInValue(e.target.value)} placeholder="Add a note here"/>
+                <input type="text" value={inValue} autoFocus onChange={e => setInValue(e.target.value)} placeholder="Add a note here" />
+                <div className="check">
+                    <label htmlFor="importance">important</label>
+                    <input type="checkbox" name="importance" id="importance" />
+                </div>
                 <button>Add Note</button>
             </form>
+            <div className="middButton"><button onClick={handleFilter}>{actFilter ? 'Show all' : 'Filter important'}</button></div>
+            {/* <button className="middButton">Filter important</button> */}
             <p>&copy; EDUARDO DANIEL URBINA MARTINEZ &copy;</p>
         </div>
     )
 }
 
-function NoteLine({note}){
-    const {content, important} = note 
-    return(
+function NoteLine({ note }) {
+    const { content, important } = note
+    return (
         <tr>
             <td>{content}</td>
-            <td className={important?'impCeld':''}><span className={note.important ? "important" : 'not-important'}>{note.important?'important':'not-important'}</span></td>
+            <td className={important ? 'impCeld' : ''}><span className={note.important ? "important" : 'not-important'}>{note.important ? 'important' : 'not-important'}</span></td>
         </tr>
     )
 }
